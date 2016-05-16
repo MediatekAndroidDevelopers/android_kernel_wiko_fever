7c2434cfc31a518de371f5ff56bfeb9338527e73
yang-cy.chen <yang-cy.chen@mediatek.com>
2016-05-16 17:09:45 +0800

    Fix "buffer overflow in Mediatek HWMON driver" issue
    Problem:
    lack of boundary check of user input parameter
    
    Solution:
    Add boundary protection to prevent buffer overflow
    
    Bug num:28174490
    
    Signed-off-by: yang-cy.chen <yang-cy.chen@mediatek.com>
    (cherry picked from commit 33370592f9b9c50be21d375229b2710904de928a)
    
    Change-Id: I2736b2d0846751a70b6b63607d536e676218ad72
    Ticket: PORRIDGE-398

--- drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
+++ drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
@@ -604,6 +604,11 @@ static int hwmsen_enable(struct hwmdev_object *obj, int sensor, int enable)
 
 	sensor_type = 1LL << sensor;
 
+	if (sensor > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+	}
+
 	if (!obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
 		return -EINVAL;
@@ -719,6 +724,11 @@ static int hwmsen_enable_nodata(struct hwmdev_object *obj, int sensor, int enabl
 	HWM_FUN(f);
 	sensor_type = 1LL << sensor;
 
+	if (sensor > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+	}
+
 	if (NULL == obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
 		return -EINVAL;
@@ -773,6 +783,11 @@ static int hwmsen_set_delay(int delay, int handle)
 {
 	int err = 0;
 	struct hwmsen_context *cxt = NULL;
+
+	if (handle > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", handle);
+		return -EINVAL;
+	}
 
 	cxt = hwm_obj->dc->cxt[handle];
 	if (NULL == cxt || (cxt->obj.sensor_operate == NULL)) {
