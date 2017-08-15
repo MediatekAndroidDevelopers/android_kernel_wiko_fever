a1dfb714da680943912ef421ce2d2e79c36e0f55
fire855 <thefire855@gmail.com>
2017-08-15 11:36:04 +0200

    Revert "smartwake: be more tolerant"
    This reverts commit e89a034e48fbf89279b23a2bcaea1f834dcd0eba.

--- drivers/input/touchscreen/smartwake.c
+++ drivers/input/touchscreen/smartwake.c
@@ -267,15 +267,14 @@ static bool gesture_is_c(int count)
 		{
 			sum += passed_gestures[i];
 		}
-		
-                if ((sum == LEFT + LEFT_DOWN + DOWN + RIGHT_DOWN + RIGHT) ||
-                        (sum == LEFT_DOWN + DOWN + RIGHT_DOWN + RIGHT) ||
-                        (sum == LEFT + LEFT_DOWN + DOWN + RIGHT_DOWN) ||
-                        (sum == LEFT_DOWN + DOWN + RIGHT_DOWN))
-                {
-                        return true;
-                }
 	}
+        if ((sum == LEFT + LEFT_DOWN + DOWN + RIGHT_DOWN + RIGHT) ||
+                (sum == LEFT_DOWN + DOWN + RIGHT_DOWN + RIGHT) ||
+                (sum == LEFT + LEFT_DOWN + DOWN + RIGHT_DOWN) ||
+                (sum == LEFT_DOWN + DOWN + RIGHT_DOWN))
+        {
+                return true;
+        }
 	return false;
 }
 
@@ -344,7 +343,7 @@ static void check_gesture(void)
                 {
                         key_value = KEY_M;
                 }
-        } else if (gesture_count < 7 && key_value == 0) {
+        } else if (key_value == 0) {
                 if (gesture_is_c(gesture_count))
                 {
                         key_value = KEY_C;
